59e71636f8ec4e065fab403d6821a9b7049a266a
Yann CLS <cleonisyann@icloud.com>
2022-09-23 08:15:56 +0200

    hide console.log

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,8 +4,8 @@ module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-    console.log("decodedToken Id", decodedToken.userId);
-    console.log("decodedToken Admin", decodedToken.isAdmin);
+    // console.log("decodedToken Id", decodedToken.userId);
+    // console.log("decodedToken Admin", decodedToken.isAdmin);
 
     const userId = decodedToken.userId;
     req.userId = userId;
